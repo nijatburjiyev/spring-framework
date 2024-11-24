1d3e38562f4a4d6f34e64ab2641419698b4542e2
nijatburjiyev <nijatburjiyev@gmail.com>
2024-11-23 21:06:17 -0800

    Thymeleaf

---
+++ Spring-06-Spring-Demo/src/main/java/org/example/Spring06SpringDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring06SpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring06SpringDemoApplication.class, args);
+    }
+
+}
