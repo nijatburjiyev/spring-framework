56c73bdcc0b893b1071b4c016eab0501fb8031b1
nijatburjiyev <nijatburjiyev@gmail.com>
2024-12-20 10:49:18 -0800

    H2 Browser level database configuration

---
+++ Spring-10-ORM-TableCreation/src/test/java/org/example/spring10ormtablecreation/Spring10OrmTableCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring10ormtablecreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring10OrmTableCreationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
